abdbba44b0ae5d8b7914dffe6f8e7005c3b93ca7
wagasenavicursor <wagasenavi.cursor@gmail.com>
2026-09-18 22:15:45 +0530

    Add department repository

---
+++ backend/src/main/java/com/ptms/repository/DepartmentRepository.java
@@ -0,0 +1,1 @@
+package com.ptms.repository; import com.ptms.domain.Department; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface DepartmentRepository extends JpaRepository<Department,Long>{List<Department> findAllByOrderByNameAsc(); Optional<Department> findByNameIgnoreCase(String name);}
